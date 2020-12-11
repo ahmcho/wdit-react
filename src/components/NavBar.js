a8ef5d3d8dd2daa225237c4fdf75cab66a026170
ahmcho <mail@ahmcho.com>
2020-12-11 15:32:09 +0400

    Added NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
+  const [token, setToken] = useState('');
+  useEffect( () => {
+    const acquiredToken = localStorage.getItem("token");
+    if(acquiredToken){
+      setToken(acquiredToken);
+    }
+  },[]);
+
     return(
         <ul>
-            <li>My Account</li>
-            <li>Login</li>
-            <li>Register</li>
-            <li>Logout</li>
-        </ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          { !token ? (
+            <div>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </div>
+          ) : (
+            <li>
+              <Link to="/dashboard">My Account</Link>
+            </li>
+          )}
+      </ul>
     )
 }
 
