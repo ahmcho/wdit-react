3fcf37dfb0b34db4a4eecaf89d8484d38c4a439c
ahmcho <mail@ahmcho.com>
2020-12-11 15:34:13 +0400

    Added MyAcoount component

---
+++ src/containers/MyAccount.js
@@ -0,0 +1,44 @@
+import React, {useEffect, useState} from 'react';
+import jwt_decode from 'jwt-decode';
+import Form from '../components/Form';
+
+const MyAccount = () => {
+    const [name, setName] = useState('');
+
+    useEffect(() => {
+        let token = localStorage.getItem('token');
+        if(token){
+            const decoded = jwt_decode(token);
+            const name = decoded.name;
+            setName(name);
+        }
+    }, []);
+
+    const handleLogout = () => {
+        let token = localStorage.getItem('token');
+        if(!token){
+            this.disabled = true;
+        } else {
+            localStorage.removeItem('token');
+        } 
+        window.location.href = "/"
+    }
+
+    return (
+        <div>
+            {name 
+                ? (
+                    <div>
+                        <p>Welcome, {name}!</p>
+                        <Form />
+                        <button onClick={handleLogout}>Log out</button>
+                    </div>
+                ) : (
+                    <p>Unauthorized</p>
+                )
+            }
+        </div>
+    )
+}
+
+export default MyAccount;
