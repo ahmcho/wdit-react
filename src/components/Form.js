0065ada89c68aeacac0d48122f51a09e543eb1d3
ahmcho <mail@ahmcho.com>
2020-12-11 15:33:20 +0400

    Added Form component

---
+++ src/components/Form.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import wditAPI from '../api/wdit';
+
+const Form = () => {
+    const [description, setDescription] = useState('');
+    const [location, setLocation] = useState('');
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        await wditAPI.post('/api/trips', { description, location });
+        window.location.href = "/";
+    }
+    const handleDescription = (e) => {
+        setDescription(e.target.value)
+    }
+    const handleLocation = (e) => {
+        setLocation(e.target.value)
+    }
+    return(
+        <div>
+            <h1>Add a new trip</h1>
+            <form onSubmit={onSubmit}>
+                <input type="text" placeholder="Description" value={description} onChange={handleDescription} />
+                <input type="text" placeholder="Location" value={location} onChange={handleLocation} />
+                <button>Add a trip</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form;
