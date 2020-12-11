6394e4d91478e71562cc30a08988fa621d479581
ahmcho <mail@ahmcho.com>
2020-12-11 15:30:59 +0400

    Added placeholder for Redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
 import Landing from './containers/Landing';
-//import NavBar from './components/NavBar';
-import 'fontsource-roboto';
-function App() {
+import NavBar from './components/NavBar';
+import About from './containers/About';
+import MyAccount from './containers/MyAccount';
+
+// import { Provider } from 'react-redux';
+// import store from './store';
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const  App = () => {
   return (
-    <div className="App">
-        <Landing />
-    </div>
+    <Router>
+      <NavBar />
+      <Switch>
+          <Route exact path="/">
+            <Landing />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <MyAccount />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
