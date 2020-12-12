360f790847c96dbf9c2e13fef2a36dc33f647983
ahmcho <mail@ahmcho.com>
2020-12-12 15:42:35 +0400

    Added auth components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Landing from './containers/Landing';
 import NavBar from './components/NavBar';
 import About from './containers/About';
 import MyAccount from './containers/MyAccount';
+import Login from './containers/Login';
+import Register from './containers/Register';
 
 // import { Provider } from 'react-redux';
 // import store from './store';
@@ -14,15 +16,11 @@ const  App = () => {
     <Router>
       <NavBar />
       <Switch>
-          <Route exact path="/">
-            <Landing />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/dashboard">
-            <MyAccount />
-          </Route>
+          <Route exact path="/" component={Landing} />
+          <Route path="/about" component={About} />
+          <Route path="/dashboard" component={MyAccount} />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
         </Switch>
     </Router>
   );
