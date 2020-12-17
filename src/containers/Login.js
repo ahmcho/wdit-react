65009ebd7bf45fcea74767465b57c84dbab30a53
ahmcho <mail@ahmcho.com>
2020-12-17 13:49:48 +0400

    Added error message display

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -5,6 +5,7 @@ import wditAPI from '../api/wdit';
 const Login = (props) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
     const onSubmit = async (e) => {
         e.preventDefault();
@@ -13,17 +14,22 @@ const Login = (props) => {
                 email,
                 password
             })
-            const token = res.data.data;
-            await localStorage.setItem('token', token);
-            window.location = "/";
+            if(res){
+                const token = res.data.data;
+                await localStorage.setItem('token', token);
+                window.location = "/";
+            }
         } catch (error) {
-            throw new Error(error.message);
+            setErrorMessage(error);
         }
     }
 
     return(
        <div>
             <p>Sign in to your account</p>
+                {errorMessage && (
+                    <p style={{color: 'red'}}>{errorMessage}</p>
+                )}
             <form onSubmit={onSubmit}>
                 <input type="email" placeholder="Your email" value={email} onChange={(e) => setEmail(e.target.value)} />
                 <input type="password" placeholder="Your password" value={password} onChange={(e) => setPassword(e.target.value)} />
