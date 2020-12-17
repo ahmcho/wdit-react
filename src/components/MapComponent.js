c328755ce80a485a20d7a0500fcdd015b6024849
ahmcho <mail@ahmcho.com>
2020-12-17 16:49:54 +0400

    Extracted markers array to MarkerList component

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,79 +1,35 @@
-import React, {useEffect, useState, useCallback, memo} from 'react';
-import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
+import React, { useState, memo} from 'react';
+import { GoogleMap, LoadScript, InfoWindow } from '@react-google-maps/api';
+import MarkerList from '../components/MarkerList';
+import getCurrentLocation from '../utils/getCurrentLocation';
 
 const containerStyle = {
   width: '100%',
   height: '400px'
 };
 
+let center;
+
+getCurrentLocation().then((data) => {
+  center = data;
+})
+
 const MapComponent = ({ trips }) => { 
-  const [map, setMap] = useState(null);
   const [ selected, setSelected ] = useState({});
-  const [ currentPosition, setCurrentPosition ] = useState({});
 
   const onSelect = item => setSelected(item);
 
-  const onLoad = useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
-    setMap(map)
-  }, []);
-
-  const onUnmount = useCallback(function callback(map) {
-    setMap(null)
-  }, []);
-
-  const getPosition = () => {
-    return new Promise((res, rej) => {
-        navigator.geolocation.getCurrentPosition(res, rej);
-    });
-  }
-
-  const setPositionFromGeolocation = async () => {
-    try {
-      const res = await	getPosition(); // wait for getPosition to complete
-      const currentPosition = {
-        lat: res.coords.latitude,
-        lng: res.coords.longitude
-      }
-      setCurrentPosition(currentPosition);
-    } catch (error) {
-      console.error(error);
-    }
-    
-  }
-  
-  useEffect(() => {
-    setPositionFromGeolocation(); 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[trips]);
-  
     return (
-       <LoadScript
-      googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
-    >
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={currentPosition}
-        zoom={2}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
+      <LoadScript
+        googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
       >
-        {Array.from(trips).map(trip => {
-            const position = {
-              lng: trip.geometry.coordinates[0],
-              lat: trip.geometry.coordinates[1]
-            }
-              return (
-              <Marker 
-                key={trip._id}
-                position={position}
-                onClick={() => onSelect(trip)}
-                />
-              )
-            })
-        }
-        {
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={4.5}
+        >
+          <MarkerList markers={trips} onSelect={onSelect}/>
+          {
             selected.geometry && (
               <InfoWindow
               position={{
@@ -86,11 +42,9 @@ const MapComponent = ({ trips }) => {
               <p>{selected.description}</p>
             </InfoWindow>
             )
-         }
-        
+          }
       </GoogleMap>
     </LoadScript>
-      
     );
 }
 
