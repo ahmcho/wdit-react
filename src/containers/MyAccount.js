494f94a5dadc21a646d3a6d5c9acdf0d22f26d5d
ahmcho <mail@ahmcho.com>
2020-12-22 12:43:05 +0400

    Props for TripForm

--- src/containers/MyAccount.js
+++ src/containers/MyAccount.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import {Redirect} from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import TripForm from '../components/TripForm';
 
@@ -11,6 +12,8 @@ const MyAccount = () => {
             const decoded = jwt_decode(token);
             const name = decoded.name;
             setName(name);
+        } else {
+            setName(null);
         }
     }, []);
 
@@ -30,7 +33,10 @@ const MyAccount = () => {
                 ? (
                     <div>
                         <p>Welcome, {name}!</p>
-                        <TripForm />
+                        <TripForm 
+                            formTitle="Add a trip"
+                            buttonTitle="Create"
+                        />
                         <button onClick={handleLogout}>Log out</button>
                     </div>
                 ) : (
