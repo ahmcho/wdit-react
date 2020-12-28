f84a6d7f586c709114a41fbba52b92f48f3eeb8a
ahmcho <mail@ahmcho.com>
2020-12-28 18:31:41 +0400

    Added map configuration files

---
+++ src/config/settings.js
@@ -0,0 +1,19 @@
+import styles from './styles';
+
+export const libraries = ["places"];
+
+export const mapContainerStyle = {
+    width: '100vw',
+    height: '50vh'
+};
+
+export const center = {
+    lat: 40.3668992,
+    lng: 49.81719040000001
+};
+
+export const options={
+    styles,
+    disableDefaultUI: true,
+    zoomControl: true
+};
