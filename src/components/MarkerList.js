60637784111afa91e1546e6282dcc5094251f47c
ahmcho <mail@ahmcho.com>
2020-12-28 18:29:23 +0400

    Fixed code layout

--- src/components/MarkerList.js
+++ src/components/MarkerList.js
@@ -4,16 +4,22 @@ import { Marker } from '@react-google-maps/api';
 const MarkerList = ({markers, onSelect}) => (
     <>
         {Array.from(markers).map(trip => {
-        const position = {
-          lng: trip.geometry.coordinates[0],
-          lat: trip.geometry.coordinates[1]
-        }
-        return <Marker 
-                key={trip._id}
-                position={position}
-                onClick={() => onSelect(trip)}
+            const position = {
+              lng: trip.geometry.coordinates[0],
+              lat: trip.geometry.coordinates[1]
+            }
+            return <Marker 
+                    key={trip._id}
+                    position={position}
+                    onClick={() => onSelect(trip)}
+                    icon={{
+                      url: '/luggage.png',
+                      scaledSize: new window.google.maps.Size(30,30),
+                      origin: new window.google.maps.Point(0,0),
+                      anchor: new window.google.maps.Point(15,15)
+                    }}
             />
-    })}
+        })}
     </>
 )
 
