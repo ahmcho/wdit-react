45c7ccc157a56ae8ecf2f2bf5480f3736e55541e
ahmcho <mail@ahmcho.com>
2020-12-29 17:02:14 +0400

    Fix for Redux

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,31 +1,33 @@
 const errorHandler = (error) => {
-    let message = '';
-    if(error.includes('email')){
-        if(error.includes('in use')){
-            return error;
+    if (typeof error === 'string') {
+        let message = '';
+        if (error.includes('email')) {
+            if (error.includes('in use')) {
+                return error;
+            }
+            message += 'Email is required! ';
         }
-        message += 'Email is required! ';
-    }
-    if(error.includes('password')){
-        switch(error.includes('password')){
-            case error.includes('not a secure'):
-                message += 'Password is not secure! ';
-                break;
-            case error.includes('shorter'):
-                message += 'Password is short!';
-                break;
-            default:
-                message += 'Password is required! ';
+        if (error.includes('password')) {
+            switch (error.includes('password')) {
+                case error.includes('not a secure'):
+                    message += 'Password is not secure! ';
+                    break;
+                case error.includes('shorter'):
+                    message += 'Password is short!';
+                    break;
+                default:
+                    message += 'Password is required! ';
+            }
         }
-    } 
-    if(error.includes('name')){
-        message += 'Name is required! ';
-    }
-    if(error.includes('age')){
-        message += error.split('age: ')[1];
-    }
+        if (error.includes('name')) {
+            message += 'Name is required! ';
+        }
+        if (error.includes('age')) {
+            message += error.split('age: ')[1];
+        }
 
-    return message;
+        return message;
+    }
 }
 
 export default errorHandler;
