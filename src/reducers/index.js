cb607b2a59b35b2a1c67a4d81ed4de370b3fa72e
ahmcho <mail@ahmcho.com>
2021-01-06 15:56:10 +0400

    Modified to include trips reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import auth from "./auth";
 import error from "./error";
+import trips from "./trips";
 
 export default combineReducers({
   auth,
-  error
+  error,
+  trips
 });
