cb1269143dcfb6031526f2f3db20049384c8926c
ahmcho <mail@ahmcho.com>
2021-01-13 18:07:06 +0400

    Removed unnecessary console.log

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useEffect} from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { connect } from "react-redux";
+import {getTrips} from '../actions/trips';
 import MapComponent from '../components/MapComponent';
 
 
-const Landing = ({auth, trips}) => {
-    let areTripsEmpty = Object.keys(trips.trips).length === 0;
+const Landing = ({auth, trips, error}) => {
+    const history = useHistory(); 
+    useEffect(() => {
+        return history.listen((location,action) => {
+            return auth.isAuthenticated ? getTrips() : null;
+        })
+    },[auth, history, trips]);
+    
+    let areTripsEmpty = trips.length === 0;
     return (
         <section>
             <h1>WhereDidITravel?</h1>
@@ -13,7 +21,7 @@ const Landing = ({auth, trips}) => {
                 <>
                     {!areTripsEmpty ? (
                         <>
-                            <MapComponent trips={trips.trips}/>
+                            <MapComponent trips={trips}/>
                             <small>Zoom out to see all trips</small>
                         </>
                     ) : 'No trips found'}
@@ -25,9 +33,11 @@ const Landing = ({auth, trips}) => {
 
 const mapStateToProps = state => ({
     auth: state.auth,
+    error: state.error,
     trips: state.trips
 });
   
 export default connect(
-    mapStateToProps
+    mapStateToProps,
+    {getTrips}
 )(Landing);
