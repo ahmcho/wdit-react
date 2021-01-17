f9533e38acd899b51f5737f62416ec7b6404c1d1
ahmcho <mail@ahmcho.com>
2021-01-17 16:05:54 +0400

    Added styling and minor fixes

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -1,11 +1,27 @@
 import React, {useEffect} from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link as RouterLink, useHistory, Redirect } from 'react-router-dom';
 import { connect } from "react-redux";
 import {getTrips} from '../actions/trips';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+
 import MapComponent from '../components/MapComponent';
 
+const useStyles = makeStyles((theme) => ({
+    typographyStyle: {
+        flexGrow: 1,
+        textAlign: "center",
+    },
+    link:{
+        justifyContent: "center",
+        textAlign: "center",
+    }
+}));
 
 const Landing = ({auth, trips, error}) => {
+    const classes = useStyles();
     const history = useHistory(); 
     useEffect(() => {
         return history.listen((location,action) => {
@@ -15,19 +31,27 @@ const Landing = ({auth, trips, error}) => {
     
     let areTripsEmpty = trips.length === 0;
     return (
-        <section>
-            <h1>WhereDidITravel?</h1>
+        <Grid container>
             {auth.isAuthenticated ? (
                 <>
                     {!areTripsEmpty ? (
                         <>
                             <MapComponent trips={trips}/>
-                            <small>Zoom out to see all trips</small>
+                            <Typography className={classes.typographyStyle} variant="subtitle2">Zoom out to see all trips</Typography>
                         </>
-                    ) : 'No trips found'}
+                    ) : (
+                        <>
+                            <Grid item xs={12}>
+                                <Typography className={classes.typographyStyle} variant="h6">No trips found</Typography>
+                            </Grid>
+                            <Grid align="center" item xs={12}>
+                                <Link  variant="body2" component={RouterLink} to="/dashboard">{"Add a trip"}</Link>
+                            </Grid>
+                        </>
+                    )}
                 </>
-            ):(<Link to="/login">Log In</Link>)}
-        </section>
+            ):(<Redirect to="/login"/>)}
+        </Grid>
     )
 }
 
