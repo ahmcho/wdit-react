4d75af51252b93b47de5623d8af01605936b7325
ahmcho <mail@ahmcho.com>
2021-01-21 18:25:19 +0400

    Remove unnecessary USER_LOADING case

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,11 +1,10 @@
-import { SET_CURRENT_USER, USER_LOADING } from "../actions/types";
+import { SET_CURRENT_USER } from "../actions/types";
 
 const isEmpty = require("is-empty");
 
 const initialState = {
   isAuthenticated: false,
-  user: {},
-  loading: false
+  user: {}
 };
 
 const auth = (state = initialState, action) => {
@@ -15,11 +14,6 @@ const auth = (state = initialState, action) => {
         ...state,
         isAuthenticated: !isEmpty(action.payload),
         user: action.payload
-      };
-    case USER_LOADING:
-      return {
-        ...state,
-        loading: true
       };
     default:
       return state;
