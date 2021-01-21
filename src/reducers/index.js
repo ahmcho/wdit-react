e04775d005272d5c468e90e45b874911bfc2afc0
ahmcho <mail@ahmcho.com>
2021-01-21 18:29:36 +0400

    Add ui reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import auth from "./auth";
 import error from "./error";
+import ui  from './ui';
 import trips from "./trips";
 
 export default combineReducers({
   auth,
   error,
-  trips
+  trips,
+  ui
 });
