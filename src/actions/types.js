97c995bd4f588772eeda07e3ac1c67e2ffca44af
ahmcho <mail@ahmcho.com>
2021-01-21 18:20:04 +0400

    Add store reset type

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,7 +1,9 @@
 export const GET_ERRORS = "GET_ERRORS";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
-export const USER_LOADING = "USER_LOADING";
+export const IS_LOADING = "IS_LOADING";
+export const DONE_LOADING = "DONE_LOADING";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
+export const RESET = "RESET";
 
 //Trips
 export const GET_TRIP = "GET_TRIP";
