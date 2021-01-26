04141dfbb744c50d3d4a9a74ae495d92d89a84b3
ahmcho <mail@ahmcho.com>
2021-01-26 13:19:50 +0400

    Add new types

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,9 +1,12 @@
+//User Interface
 export const GET_ERRORS = "GET_ERRORS";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
 export const IS_LOADING = "IS_LOADING";
 export const DONE_LOADING = "DONE_LOADING";
-export const SET_CURRENT_USER = "SET_CURRENT_USER";
 export const RESET = "RESET";
+export const ENQUEUE_NOTIFICATION = "ENQUEUE_NOTIFICATION";
+export const CLOSE_NOTIFICATION = "CLOSE_NOTIFICATION";
+export const REMOVE_NOTIFICATION = "REMOVE_NOTIFICATION";
 
 //Trips
 export const GET_TRIP = "GET_TRIP";
@@ -13,11 +16,10 @@ export const CREATE_TRIP = "CREATE_TRIP";
 export const DELETE_TRIP = "DELETE_TRIP";
 
 //Users
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
 export const GET_CURRENT_USER = "GET_CURRENT_USER";
 export const LOGIN_USER = "LOGIN_USER";
 export const CREATE_USER = "CREATE_USER";
 export const GET_USER_TRIPS = "GET_USER_TRIPS";
 export const UPDATE_USER = "UPDATE_USER";
 export const DELETE_USER = "DELETE_USER";
-export const USER_PASSWORD_FORGOT = "USER_PASSWORD_FORGOT"
-export const USER_PASSWORD_RESET = "USER_PASSWORD_RESET"
