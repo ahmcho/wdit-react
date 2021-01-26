b35f643c8c02d9052e33bb1f0a83cdea1b2f0f2c
ahmcho <mail@ahmcho.com>
2021-01-26 13:20:29 +0400

    Add Material UI Snackbar for better UX

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -1,14 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import errorHandler from '../utils/errorHandler';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
+
+const Alert = (props) => {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 const ErrorMessage = ({message}) => {
     const [error, setError] = useState('');
+    const [open, setOpen] = React.useState(false);
+    
+    useEffect(() => {
+        if(error.length !== 0){
+            setOpen(true);
+        }
+    },[error]);
+    
     useEffect( () => {
         setError(errorHandler(message));
-    }, [message])
+    }, [message]);
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+    
+        setOpen(false);
+    };
     return(
         <>
-            {error && (<p style={{color: 'red'}}>{error}</p>)}
+            {error.length !== 0 && (
+                <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                    <Alert onClose={handleClose} severity="error">
+                        {error}
+                    </Alert>
+                </Snackbar>
+            )}
         </>
     );
 
