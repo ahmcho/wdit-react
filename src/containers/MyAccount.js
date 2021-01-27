ef4fb72eee59d7a1ce99d29d8323c5f2c0f48156
ahmcho <mail@ahmcho.com>
2021-01-27 14:56:08 +0400

    Add tab panel component & removed unnecessary imports

--- src/containers/MyAccount.js
+++ src/containers/MyAccount.js
@@ -4,17 +4,31 @@ import {Redirect} from 'react-router-dom';
 import { createTrip } from '../actions/trips';
 import { logoutUser } from '../actions/auth';
 import TripForm from '../components/TripForm';
-
+import TabPanel from '../components/TabPanel';
+import Grid from '@material-ui/core/Grid';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import AddIcon from '@material-ui/icons/Add';
 
+  
+function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
 const MyAccount = ({auth, error, logoutUser}) => {
-    const [name, setName] = useState('');
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
     
     useEffect(() => {
         error.toString().includes('Unauthorized') && logoutUser();
-        auth.isAuthenticated && ( setName(auth.user.name) )
     }, [auth, error,logoutUser]);
 
 
@@ -25,17 +39,32 @@ const MyAccount = ({auth, error, logoutUser}) => {
             )}
             {auth.isAuthenticated 
                 ? (
-                    <>
-                        <Typography variant="h4" align="center">
-                            Welcome, {name}
-                        </Typography>
-                        <TripForm 
-                            formTitle="Add a trip"
-                            buttonTitle="Create"
-                            startIcon={<AddIcon />}
-                            onSubmit={createTrip}
-                        />
-                    </>
+                    <Grid container justify="center" alignItems="center"  spacing={3}>
+                        <Grid item xs={12}>
+                            <Typography  variant="h4" align="center">
+                                Welcome to dashboard
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <AppBar position="static">
+                                <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+                                <Tab label="Dashboard" {...a11yProps(0)} />
+                                <Tab label="Settings" {...a11yProps(2)} />
+                                </Tabs>
+                            </AppBar>
+                            <TabPanel value={value} index={0}>
+                                <TripForm 
+                                    formTitle="Add a trip"
+                                    buttonTitle="Create"
+                                    startIcon={<AddIcon />}
+                                    onSubmit={createTrip}
+                                />
+                            </TabPanel>
+                            <TabPanel value={value} index={1}>
+                                Some settings placeholder
+                            </TabPanel>
+                        </Grid>
+                    </Grid>
                 ) : (
                     <Redirect to='/login' />
                 )
