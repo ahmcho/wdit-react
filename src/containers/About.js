2e75720c22187876e8aa3d7c63740864262fe3f5
ahmcho <mail@ahmcho.com>
2021-01-28 20:25:30 +0400

    Remove unused imports

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,21 +1,8 @@
 import React from 'react';
-
-import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Link from '@material-ui/core/Link';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles((theme) => ({
-    typographyStyle: {
-        flexGrow: 1,
-        textAlign: "center",
-    },
-    link:{
-        justifyContent: "center",
-        textAlign: "center",
-    }
-}));
 
 const About = () => {
     return (
