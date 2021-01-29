d59a36b949c8f505ba36eb0ff9fc3b5d0f8c3ee2
ahmcho <mail@ahmcho.com>
2021-01-29 14:23:56 +0400

    Add TripList component, modify tab component, remove styles to separate file

--- src/containers/MyAccount.js
+++ src/containers/MyAccount.js
@@ -10,13 +10,16 @@ import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Container from '@material-ui/core/Container';
+import TripList from '../components/TripList';
+import useStyles from '../config/styles';
 //Icons
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import SettingsIcon from '@material-ui/icons/Settings';
-import DashboardIcon from '@material-ui/icons/Dashboard';
+import ListIcon from '@material-ui/icons/List';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
+import { Paper, Typography } from '@material-ui/core';
 
   
 function a11yProps(index) {
@@ -25,8 +28,9 @@ function a11yProps(index) {
       'aria-controls': `simple-tabpanel-${index}`,
     };
 }
-const MyAccount = ({auth, error, logoutUser}) => {
+const MyAccount = ({auth, trips, error, logoutUser}) => {
     const [value, setValue] = useState(0);
+    const classes = useStyles();
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
@@ -47,34 +51,43 @@ const MyAccount = ({auth, error, logoutUser}) => {
                     <Grid container justify="center" alignItems="center"  spacing={3}>
                         <Grid item xs={12}>
                             <AppBar position="static">
-                                <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
-                                <Tab label="Dashboard" {...a11yProps(0)} icon={<DashboardIcon />} />
+                                <Tabs variant="fullWidth" value={value} onChange={handleChange} aria-label="simple tabs example">
+                                <Tab label="Manage" {...a11yProps(0)} icon={<ListIcon />} />
+                                <Tab label="Create" {...a11yProps(1)} icon={<AddIcon />} />
                                 <Tab label="Settings" {...a11yProps(2)} icon={<SettingsIcon />} />
                                 </Tabs>
                             </AppBar>
                             <TabPanel value={value} index={0}>
-                                <TripForm 
-                                    formTitle="Add a trip"
-                                    buttonTitle="Create"
-                                    startIcon={<AddIcon />}
-                                    onSubmit={createTrip}
-                                />
+                                <Container maxWidth="sm" spacing={3}>
+                                    <div className={classes.paper}>
+                                        <Grid container justify="center" alignItems="center"  spacing={3}>
+                                            <Grid item xs={12}>
+                                                <Paper className={classes.paper}>
+                                                    <Typography variant="h4">
+                                                        Manage your trips
+                                                    </Typography>
+                                                </Paper>
+                                            </Grid>
+                                            <TripList trips={trips} />
+                                        </Grid>
+                                    </div>
+                                </Container>
                             </TabPanel>
                             <TabPanel value={value} index={1}>
+                                <TripForm 
+                                        formTitle="Add a trip"
+                                        buttonTitle="Create"
+                                        startIcon={<AddIcon />}
+                                        onSubmit={createTrip}
+                                />
+                            </TabPanel>
+                            <TabPanel value={value} index={2}>
                                 <Grid container justify="center" alignItems="center"  spacing={3}>
                                     <Grid item xs={12}>
                                         <Button
                                             height={60}
                                             variant="contained"
-                                            color="primary"
-                                            style={{ margin: "1ch"}}
-                                            endIcon={<ExitToAppIcon />}
-                                        > 
-                                            Logout    
-                                        </Button>
-                                        <Button
-                                            height={60}
-                                            variant="contained"
+                                            className={classes.delete}
                                             color="primary"
                                             endIcon={<DeleteForeverIcon />}
                                         > 
@@ -95,7 +108,8 @@ const MyAccount = ({auth, error, logoutUser}) => {
 
 const mapStateToProps = state => ({
     auth: state.auth,
-    error: state.error
+    error: state.error,
+    trips: state.trips
 });
   
 export default connect(
