87c86b861dfc5f5135054135ca3a27f3b90a4ff4
ahmcho <mail@ahmcho.com>
2021-01-30 16:17:15 +0400

    Add delete user function to button click

--- src/containers/MyAccount.js
+++ src/containers/MyAccount.js
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import { connect } from "react-redux";
 import {Redirect} from 'react-router-dom';
-import { createTrip } from '../actions/trips';
-import { logoutUser } from '../actions/auth';
+import { createTrip,getTrips } from '../actions/trips';
+import { logoutUser, deleteUser } from '../actions/auth';
 import TripForm from '../components/TripForm';
 import TabPanel from '../components/TabPanel';
 import Grid from '@material-ui/core/Grid';
@@ -10,10 +10,11 @@ import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Container from '@material-ui/core/Container';
+//Components
+import Alert from '../components/Alert';
 import TripList from '../components/TripList';
 import useStyles from '../config/styles';
 //Icons
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import SettingsIcon from '@material-ui/icons/Settings';
 import ListIcon from '@material-ui/icons/List';
@@ -28,18 +29,37 @@ function a11yProps(index) {
       'aria-controls': `simple-tabpanel-${index}`,
     };
 }
-const MyAccount = ({auth, trips, error, logoutUser}) => {
+const MyAccount = ({auth, trips, error, logoutUser, deleteUser}) => {
     const [value, setValue] = useState(0);
+    const [open, setOpen] = React.useState(false);
     const classes = useStyles();
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
-    
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleAgree = () => {
+        setOpen(false);
+        //delete account
+        deleteUser(); 
+    }
+
+    const handleDisagree = () => {
+        setOpen(false);
+        //dont delete account
+    }
+
     useEffect(() => {
         error.toString().includes('Unauthorized') && logoutUser();
     }, [auth, error,logoutUser]);
-
 
     return (
         <Container maxWidth="sm" spacing={3}>
@@ -89,10 +109,19 @@ const MyAccount = ({auth, trips, error, logoutUser}) => {
                                             variant="contained"
                                             className={classes.delete}
                                             color="primary"
+                                            onClick={handleClickOpen}
                                             endIcon={<DeleteForeverIcon />}
                                         > 
                                             Delete your account    
                                         </Button>
+                                        <Alert 
+                                            open={open}
+                                            text="This action is permanent! Your account and all trips associated with it will be removed!"
+                                            title="Are you sure?"
+                                            handleClose={handleClose}
+                                            handleDisagree={handleDisagree}
+                                            handleAgree={handleAgree}
+                                        />
                                     </Grid>
                                 </Grid>
                             </TabPanel>
@@ -114,6 +143,6 @@ const mapStateToProps = state => ({
   
 export default connect(
     mapStateToProps,
-    {createTrip, logoutUser }
+    {createTrip, logoutUser, deleteUser }
 )(MyAccount);
   
