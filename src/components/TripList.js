2ede569caeac564e3ebce0e877b1dc3c87fc3325
ahmcho <mail@ahmcho.com>
2021-02-02 21:15:37 +0400

    Add pagination for trips

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import { Link } from 'react-router-dom';
 import useStyles from '../config/styles';
 import List from "@material-ui/core/List";
@@ -6,33 +6,41 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import Divider from "@material-ui/core/Divider";
+import Pagination from "@material-ui/lab/Pagination";
 import CardTravelIcon from "@material-ui/icons/CardTravel";
-
+import paginate from '../utils/paginate';
 
 
 const TripList = ({trips}) => {
     const classes = useStyles();
-    const listItems = Object.values(trips);
+    const tripsArray = Object.values(trips);
+    const [page, setPage] = useState(1);
+
+    const onPageChange = (event, value) => {
+        setPage(value);
+    }
+
     return (
-        <div className={classes.listRoot}>
+        <div className={classes.tripListRoot}>
           <List component="nav" aria-label="main mailbox folders">
-            {listItems.map((item,index) => {
+            {paginate(tripsArray, 5, page).map((trip,index) => {
                 return(
                     <ListItem
                         button
                         component={Link}
                         key={index}
-                        to={`/trips/${item._id}`}
+                        to={`/trips/${trip._id}`}
                     >
                         <ListItemIcon>
-                            <CardTravelIcon />
+                            <CardTravelIcon color="primary"/>
                         </ListItemIcon>
-                        <ListItemText primary={item.description} />
+                        <ListItemText primary={trip.description} />
                     </ListItem>
                 )
             })}
           </List>
           <Divider />
+          <Pagination color="primary"  count={Math.ceil(tripsArray.length/5)} page={page} onChange={onPageChange} />
         </div>
     );
 }
